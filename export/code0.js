237839a212e94a866ad394a0540281a0a5e3703f
Wilky <arwilkinsonffc@gmail.com>
2021-02-28 17:38:55 +0000

    Example export

---
+++ export/code0.js
@@ -0,0 +1,20 @@
+gdjs.LevelCode = {};
+
+gdjs.LevelCode.conditionTrue_0 = {val:false};
+gdjs.LevelCode.condition0IsTrue_0 = {val:false};
+
+
+gdjs.LevelCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.LevelCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.LevelCode.eventsList0(runtimeScene);
+return;
+
+}
+
+gdjs['LevelCode'] = gdjs.LevelCode;
